dbf4c0454cf5dc56011c2b4332da78d4542e3c62
Keith Plaisance <keithplaisance88@gmail.com>
2018-11-10 18:06:43 -0700

    Game working - adding modal for game over / replay option

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y + 55;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // If enemy is not passed boundary
+    if (this.x < this.boundary) {
+        // Move forward
+        // Increment x by speed * dt
+        this.x += this.speed * dt;
+    }
+
+    else {
+        // Reset pos to start
+        this.x = this.resetPos;
+    }
+        
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +43,101 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+// Hero Class
+
+class Hero {
+    constructor() {
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 4) + 55;
+        this.x = this.startX;
+        this.y = this.startY;
+        this.sprite = "images/char-boy.png";
+        this.triumphant = false;
+    }
+
+    // Draw hero sprite on current x and y coord position
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    /**
+     * Update hero's x and y property according to input
+     * 
+     * @param {string} input - Direction to travel
+     */
+    handleInput(input) {
+        switch(input) {
+            case 'left':
+                if (this.x > 0) {
+                    this.x -= this.step;
+                }
+                break;
+            case 'up':
+                if (this.y > this.jump) {
+                    this.y -= this.jump;
+                }
+                break;
+            case 'right':
+                if (this.x < this.step * 4) {
+                    this.x += this.step;
+                }
+                break;
+            case 'down':
+                if (this.y < this.jump * 4) {
+                    this.y += this.jump;
+                }
+                break;
+        }
+    }
+    // Update position
+    update() {
+
+    // Check collision here
+    for (let enemy of allEnemies) {
+        if 
+        (this.y === enemy.y && (enemy.x + enemy.step/2 > 
+            this.x && enemy.x < this.x + this.step/2)) {
+            this.reset();
+        }
+    }
+        // Did player x and y collide with enemy?
+    // Check win here?
+        // Did player x and y reach final title?
+        if(this.y === 55) {
+            this.triumphant = true;
+        }
+
+    }
+
+    reset() {
+        this.y = this.startY;
+        this.x = this.startX;
+    }
+
+    }
+
+
+    // Constructor
+
+        // Properties
+            // x pos
+            // y pos
+            // Sprite image
+
+        // Methods
+
+            
+            // Render
+                // Draw player sprite on current x and y coord position
+
+            // Handle keyboard input
+                // Update player's x and y property according to input
+
+            // Reset Hero
+                // Set x and y to starting x and y
 
 
 // Now instantiate your objects.
@@ -44,3 +158,15 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// New Hero object
+const player = new Hero();
+const bug1 = new Enemy(-101, 0, 400);
+const bug2 = new Enemy(-101, 83, 260);
+const bug3 = new Enemy((-101*2.5), 83, 260);
+const bug4 = new Enemy((-101*5), 166, 200);
+// Init allEnemies array
+const allEnemies = [];
+// For each enemy create and push new Enemy object into above array
+allEnemies.push(bug1,bug2,bug3,bug4);
+console.log(allEnemies);
